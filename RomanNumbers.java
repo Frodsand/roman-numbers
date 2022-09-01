ca50cd6fcd6f0c1ebc4518a1065b356382baf640
Fredrik Rødsand <fredrikrod@elkjop.no>
2022-09-01 11:08:38 +0200

    🟢 NumberFour

--- RomanNumbers.java
+++ RomanNumbers.java
@@ -25,6 +25,7 @@ public class RomanNumbers {
         if (number == 1) return "I";
         if (number == 2) return "II";
         if (number == 3) return "III";
+        if (number == 4) return "IV";
         return "";
     }
 }
