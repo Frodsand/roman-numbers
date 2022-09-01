728379208dc7e5cd728d45c94ec2be6e993117dd
Fredrik Rødsand <fredrikrod@elkjop.no>
2022-09-01 10:45:15 +0200

    🟢 RomannumberisOne

--- RomanNumbers.java
+++ RomanNumbers.java
@@ -1,6 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class RomanNumbers {
     @Test
     void numberOne(){
-        assertTrue()
+        assertTrue(romanNumber(1));
+    }
+
+    private boolean romanNumber(int number){
+        return true;
     }
 }
