32ec338aa6d6c920fa98c4249504acc7794ff908
Fredrik Rødsand <fredrikrod@elkjop.no>
2022-09-01 12:00:56 +0200

    🟢 NumberTen

--- RomanNumbers.java
+++ RomanNumbers.java
@@ -44,36 +44,66 @@ public class RomanNumbers {
     void numberTen(){
         assertEquals(romanNumber(10), "X");
     }
-    private String romanNumber(int number){
+    @Test
+    void numberEleven(){
+        assertEquals(romanNumber(11), "XI");
+    }
+    @Test
+    void numberFourteen(){
+        assertEquals(romanNumber(14), "XIV");
+    }
+    private String romanNumberFour(int number){
         String total = "";
         if (number <= 4){
             if (number == 4){
-             total = "IV";
-             }
-            else{
-            for (int i = 0; i < number; i++){
-                total += "I";
-            }
-            }
-        }
-        if (number >= 5 && number <= 9){
-            total = "V";
-            if (number == 5){
-                return total;
-            }else if (number == 9){
-                return "IX";
+                total = "IV";
             }
             else{
-                for (int i = 0; i < number - 5; i++) {
-                    total = total + "I";
+                for (int i = 0; i < number; i++){
+                    total += "I";
                 }
             }
         }
-        if(number >= 10){
-            total = "X";
-            if(number == 10){
-                return total;
+        return total;
+    }
+
+    private String romanNumberFive(int number){
+       String total = "V";
+        if (number == 5){
+            return total;
+        }else if (number == 9){
+            return "IX";
+        }
+        else{
+            for (int i = 0; i < number - 5; i++) {
+                total = total + "I";
             }
+        }
+        return total;
+    }
+
+    private String romanNumberTen(int number){
+        String total = "X";
+        if(number == 10){
+            return total;
+        }
+        else{
+            for (int i = 0; i < number - 10; i++) {
+                total = total + "I";
+            }
+        }
+        return total;
+    }
+    private String romanNumber(int number){
+        String total = "";
+        if (number <= 4){
+            return romanNumberFour(number);
+        }
+        if (number >= 5 && number <= 9){
+            return romanNumberFive(number);
+        }
+        if(number >= 10){
+           return romanNumberTen(number);
         }
         return total;
 
